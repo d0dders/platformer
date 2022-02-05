2e21438a55d5fd7cc5517199f3d38c7902ec2ad5
d0dders <dodders@outlook.com>
2022-02-05 22:57:09 +0000

    Added first enemy objects

--- main.lua
+++ main.lua
@@ -9,13 +9,16 @@ function love.load()
 
     sprites = {}
     sprites.playerSheet = love.graphics.newImage('sprites/playerSheet.png')
-    
+    sprites.enemySheet = love.graphics.newImage('sprites/enemySheet.png')
+
     local grid = anim8.newGrid(614, 564, sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight())
+    local enemyGrid = anim8.newGrid(100, 79, sprites.enemySheet:getWidth(), sprites.enemySheet:getHeight())
 
     animations = {}
     animations.idle = anim8.newAnimation(grid('1-15',1), 0.05)
     animations.jump = anim8.newAnimation(grid('1-7',2), 0.05)
     animations.run = anim8.newAnimation(grid('1-15',3), 0.05)
+    animations.enemy = anim8.newAnimation(enemyGrid('1-2',1), 0.03)
 
     wf = require 'libraries/windfield/windfield'
     world = wf.newWorld(0, 800, false)
@@ -26,6 +29,7 @@ function love.load()
     world:addCollisionClass('Danger')    
     
     require('player')
+    require('enemy')
 
     --dangerZone = world:newRectangleCollider(0,550,800,50, {collision_class = 'Danger'})
     --dangerZone:setType('static')
@@ -39,6 +43,7 @@ function love.update(dt)
     world:update(dt)
     gameMap:update(dt)
     playerUpdate(dt)
+    updateEnemies(dt)
     local px, py = player:getPosition()
     cam:lookAt(px, love.graphics.getHeight()/2)
 end
@@ -48,6 +53,7 @@ function love.draw()
         gameMap:drawLayer(gameMap.layers["Tile Layer 1"])
         world:draw()
         drawPlayer()
+        drawEnemies()
     cam:detach()
 end
 
@@ -80,6 +86,9 @@ function loadMap()
     gameMap = sti("maps/level1.lua")
     for i, obj in ipairs(gameMap.layers["Platforms"].objects) do
         spawnPlatform(obj.x, obj.y, obj.width, obj.height)
+    end
+    for i, obj in ipairs(gameMap.layers["Enemies"].objects) do
+        spawnEnemy(obj.x, obj.y)
     end
 end
 
